e11a437cc7943098b48e6be868aa26c46fd1d656
Camila <camilasatie.dev@gmail.com>
2021-04-10 10:15:40 -0300

    feat: add signIn btn auth

--- 02-ignews/src/components/SignInButton/index.tsx
+++ 02-ignews/src/components/SignInButton/index.tsx
@@ -1,18 +1,23 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 import styles from './styles.module.scss';
 
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
+
+  console.log(session)
 
   const renderLoggedInButton = () => (
     <button 
       type="button"
       className={styles.btn}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Camila Satie
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} /> 
     </button>
   );
@@ -21,6 +26,7 @@ export function SignInButton() {
     <button 
       type="button"
       className={styles.btn}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
@@ -29,7 +35,7 @@ export function SignInButton() {
 
   return (
     <>
-      {isUserLoggedIn ? renderLoggedInButton : renderSignedInButton}
+      {session ? renderLoggedInButton() : renderSignedInButton()}
     </>
   )
 }
