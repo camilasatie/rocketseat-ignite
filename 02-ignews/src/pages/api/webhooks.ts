339f4be4ba7910fff370359e643cc710a456ad36
Camila <camilasatie.dev@gmail.com>
2021-04-11 17:05:54 -0300

    feat: update and delete subscription

--- 02-ignews/src/pages/api/webhooks.ts
+++ 02-ignews/src/pages/api/webhooks.ts
@@ -24,8 +24,10 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
-])
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -46,14 +48,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+
+            const subscription = event.data.object as Stripe.Subscription
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false,
+            );
+
+            break;
           case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
-          const checkoutSession = event.data.object as Stripe.Checkout.Session;
-
-          await saveSubscription(
-            checkoutSession.subscription.toString(),
-            checkoutSession.customer.toString(),
-          )
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true,
+            )
 
             break;
           default:
