d15a1066a9948f914c9f95e0a410f156f0b75a6e
Camila <camilasatie.dev@gmail.com>
2021-04-25 10:18:34 -0300

    feat: add sidebar component

--- 03-dashgo/src/components/Sidebar.tsx
+++ 03-dashgo/src/components/Sidebar.tsx
@@ -1,9 +1,40 @@
-import { Flex } from "@chakra-ui/layout";
+import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
 
 export function Sidebar() {
   return (
-    <Flex>
-      hi
-    </Flex>
+    <Box as='aside' w='64' mr='8'>
+      <Stack spacing='12' align='flex-start'>
+        <Box>
+          <Text fontSize='small' fontWeight='bold' color='gray.400'>GERAL</Text>
+          <Stack spacing='4' mt='8' align='stretch'>
+            <Link display='flex' align='center'>
+              <Icon as={RiDashboardLine} fontSize='20'/>
+              <Text ml='4' fontWeight='medium'>Dashboard</Text>
+            </Link>
+
+            <Link display='flex' align='center'>
+              <Icon as={RiContactsLine} fontSize='20'/>
+              <Text ml='4' fontWeight='medium'>Usuários</Text>
+            </Link>
+          </Stack>
+        </Box>
+
+        <Box>
+          <Text fontSize='small' fontWeight='bold' color='gray.400'>Automação</Text>
+          <Stack spacing='4' mt='8' align='stretch'>
+            <Link display='flex' align='center'>
+              <Icon as={RiInputMethodLine} fontSize='20'/>
+              <Text ml='4' fontWeight='medium'>Formulários</Text>
+            </Link>
+
+            <Link display='flex' align='center'>
+              <Icon as={RiGitMergeLine} fontSize='20'/>
+              <Text ml='4' fontWeight='medium'>Automação</Text>
+            </Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
   );
 }
