2ab5fcacbf8fce28b43fd1d831b3d91e6b4e3d59
Camila <camilasatie.dev@gmail.com>
2021-04-30 20:20:51 -0300

    feat: conditional for active link

--- 03-dashgo/src/components/Sidebar/NavLink.tsx
+++ 03-dashgo/src/components/Sidebar/NavLink.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import { Icon, Link as ChakraLink, Text, LinkProps as ChakraLinkProps} from "@chakra-ui/react";
+import { ActiveLink } from '../ActiveLink';
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: React.ElementType;
@@ -9,13 +9,13 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display='flex' align='center' {...rest}>
         <Icon as={icon} fontSize='20'/>
         <Text ml='4' fontWeight='medium'>
           { children }
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
