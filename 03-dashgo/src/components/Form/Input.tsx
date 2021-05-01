9d22565568c488c8822c8a4baa1b98d83e1363fe
Camila <camilasatie.dev@gmail.com>
2021-04-30 21:38:11 -0300

    feat: login form validation

--- 03-dashgo/src/components/Form/Input.tsx
+++ 03-dashgo/src/components/Form/Input.tsx
@@ -1,31 +1,44 @@
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from 'react-hook-form';
 import { 
   FormControl, 
   FormLabel, 
   Input as ChakraInput,
-  InputProps as ChackraInputProps 
+  InputProps as ChackraInputProps,
+  FormErrorMessage
 } from "@chakra-ui/react";
 
 interface InputProps extends ChackraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({name, label, ...rest}: InputProps) {
-  return(
-    <FormControl>
-    {label && <FormLabel htmlFor={name}>{label}</FormLabel>} 
-      <ChakraInput
-        id={name} 
-        name={name}
-        focusBorderColor='pink.500'
-        bgColor='gray.900'
-        variant='filled'
-        _hover={{
-          bgColor: 'gray.900'
-        }}
-        size='lg'
-        {...rest}
-      />
-    </FormControl> 
-  )
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> 
+  = ({name, label, error = null, ...rest}, ref) => {
+    return(
+      <FormControl isInvalid={!!error}>
+      { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>} 
+        <ChakraInput
+          id={name} 
+          name={name}
+          focusBorderColor='pink.500'
+          bgColor='gray.900'
+          variant='filled'
+          _hover={{
+            bgColor: 'gray.900'
+          }}
+          size='lg'
+          ref={ref}
+          {...rest}
+        />
+        { !!error && (
+          <FormErrorMessage>
+            {error.message}
+          </FormErrorMessage>
+        )}
+      </FormControl> 
+    )
 }
+
+export const Input = forwardRef(InputBase);
