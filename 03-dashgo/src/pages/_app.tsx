d1ee6a921621108fc7dc837650750d5298a22d53
Camila <camilasatie.dev@gmail.com>
2021-05-07 22:34:43 -0300

    feat: add react query for users page

--- 03-dashgo/src/pages/_app.tsx
+++ 03-dashgo/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 
@@ -11,13 +13,19 @@ if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
--- 03-dashgo/src/pages/users/index.tsx
+++ 03-dashgo/src/pages/users/index.tsx
@@ -12,16 +12,42 @@ import {
   Th,
   Thead,
   Tr, 
+  Spinner,
   useBreakpointValue
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
 export default function UserList() {
+  // users = nome da chave qeu será armazenada no cache
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json();
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }) 
+      };
+    });
+
+    return users;
+  }, {
+    staleTime: 1000 * 5, // seconds 
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -29,16 +55,57 @@ export default function UserList() {
 
   const renderButton = () => (
     <Button
-    as='a'
-    size='sm'
-    fontSize='sm'
-    colorScheme='purple'
-    leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-    cursor='pointer'
-  >
-    Editar
-  </Button>
+      as='a'
+      size='sm'
+      fontSize='sm'
+      colorScheme='purple'
+      leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+      cursor='pointer'
+    >
+      Editar
+    </Button>
   );
+
+  const renderTable = () => (
+    <>
+      <Table colorScheme='whiteAlpha'>
+        <Thead>
+          <Tr>
+            <Th px={['4', '4','6']} color='gray.300' width='8'>
+              <Checkbox colorScheme='pink' />
+            </Th>
+            <Th>Usuário</Th>
+            { isWideVersion && <Th>Data de cadastro</Th> }
+            <Th width='8'></Th>
+          </Tr>
+        </Thead>
+
+        <Tbody>
+          {data.map(user => {
+            return (
+              <Tr key={user.id}>
+                <Td px={['4', '4','6']}>
+                  <Checkbox colorScheme='pink' />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight='bold'>{user.name}</Text>
+                    <Text fontSize='small' color='gray.300'>{user.email}</Text>
+                  </Box>
+                </Td>
+                { isWideVersion && <Td>{user.createdAt}</Td>}
+                <Td>
+                { isWideVersion && renderButton() }
+                </Td>
+              </Tr>
+            )
+          })}
+        </Tbody>
+      </Table>
+      <Pagination />
+    </>
+  );
+
   return (
     <Box>
       <Header />
@@ -47,7 +114,11 @@ export default function UserList() {
         <Sidebar />
         <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
           <Flex mb='8' justify='space-between' align='center'>
-            <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
+              
+              { !isLoading && isFetching  && <Spinner size='sm' color='gray.500' ml='4' />}
+            </Heading>
 
             <Link href='/users/create' passHref>
               <Button
@@ -63,38 +134,17 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={['4', '4','6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de cadastro</Th> }
-                <Th width='8'></Th>
-              </Tr>
-            </Thead>
-
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4','6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Camila Satie</Text>
-                    <Text fontSize='small' color='gray.300'>camilasatie.dev@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>25 de Abril de 2021</Td>}
-                <Td>
-                 { isWideVersion && renderButton() }
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+          { 
+            isLoading ? (
+                <Flex justify='center'>
+                  <Spinner />
+                </Flex>
+            ) : error ?  (
+              <Flex justify='center'>
+                <Text>Falha ao obter dados dos usuários.</Text>
+              </Flex>
+            ) : renderTable()
+          }
         </Box>
       </Flex>
     </Box>
